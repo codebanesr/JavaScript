48283e2a0e66a671e2f1b61e43824c0f47a961f0
Shanur Rahman <shanur.cse.nitap@gmail.com>
2018-06-14 16:54:22 +0530

    async-await

---
+++ concepts/correct-way-of-using-async-await.js
@@ -0,0 +1,43 @@
+const getDetails = async function() {
+	const wes = await axios.get("https://swapi.co/api/people/1");
+	const scot = await axios.get("https://swapi.co/api/people/2");
+
+	const html = `
+		<h1>wes.name</h1>
+		<h1>scot.name</h1>
+	`
+}
+
+
+----------slow approach----------
+but thats slow why has scot got to wait for wes to finish
+
+const getDetails = async function() {
+	const wes = await axios.get("https://swapi.co/api/people/1");
+	const scot = await axios.get("https://swapi.co/api/people/2");
+
+	const html = `
+		<h1>wes.name</h1>
+		<h1>scot.name</h1>
+	`
+}
+
+
+
+
+
+--------------
+// fast approach
+
+const getDetails = async function() {
+	const wesPromise =  axios.get("https://swapi.co/api/people/1");
+	const scotPromise = axios.get("https://swapi.co/api/people/2");
+
+
+	//now it waits for both of them to complete without them waiting for it
+	const [wes, scot] = await Promise.all([wesPromise, scotPromise]);
+	const html = `
+		<h1>wes.name</h1>
+		<h1>scot.name</h1>
+	`
+}
