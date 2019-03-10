68f489892c488e913517de96d138701d428b41b9
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-03-10 22:18:08 +0530

    Update object.entries.js

--- Destructuring/object.entries.js
+++ Destructuring/object.entries.js
@@ -3,7 +3,18 @@ let user = {
   age: 30
 };
 
-// loop over keys-and-values
+// loop over keys-and-values, no entries method for dictionary in javascript use Object.entries
 for (let [key, value] of Object.entries(user)) {
   alert(`${key}:${value}`); // name:John, then age:30
+}
+
+
+
+console.log("*******************IMPORTANT Object.entries does not occur for map; instead use map.entries");
+let user = new Map();
+user.set("name", "John");
+user.set("age", "30");
+
+for(let [key, value] of user.entries()){
+  console.log(key, value)
 }
