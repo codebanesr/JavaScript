6789743102ec6ad75c7496b6141a5268d5329ff8
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-03-10 22:14:13 +0530

    Create object.entries.js
    We can simply iterate and get keys and values in javascript the way we do in python

---
+++ Destructuring/object.entries.js
@@ -0,0 +1,9 @@
+let user = {
+  name: "John",
+  age: 30
+};
+
+// loop over keys-and-values
+for (let [key, value] of Object.entries(user)) {
+  alert(`${key}:${value}`); // name:John, then age:30
+}
