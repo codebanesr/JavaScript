8b77a782f83637f150ba38cfaf04ce550db050c7
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-03-10 21:34:44 +0530

    Update setIterator.js

--- Iterating/setIterator.js
+++ Iterating/setIterator.js
@@ -1,1 +1,17 @@
 
+let s = new Set();
+s.add("shanur")
+s.add("vishnu");
+s.add("amit");
+s.add("dev");
+s.add("kumar")
+
+// console.log(s.entries())
+for(let item of s){
+  console.log(item)
+}
+
+console.log("----------m2-------")
+s.forEach(element=>{
+  console.log(element)
+})
