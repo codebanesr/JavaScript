6b88d378c1e6d97772b7ede9fa8d492ca7184017
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-03-10 16:28:58 +0530

    Create callPokemon.js

---
+++ call-bind-apply/callPokemon.js
@@ -0,0 +1,18 @@
+var object = {
+  firstName: "pika",
+  lastName: "chu",
+  
+  getPokemonName: function(){
+    let fullname = this.firstName+" "+this.lastName;
+    return fullname;
+  }
+}
+
+var pokemonEatingHabbits = function(snack1, snack2){
+  console.log(this.getPokemonName() + " I choose you");
+  console.log(this.getPokemonName() + " eats " + snack1 + " and " + snack2)
+}
+
+
+
+pokemonEatingHabbits.call(object, "cheeze", "onions");
