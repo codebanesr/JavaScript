a79bab51a1846bfef9bf1a5b79e151f7ee6aa592
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-04-08 19:36:35 +0530

    Update applyPokemon.js

--- call-bind-apply/applyPokemon.js
+++ call-bind-apply/applyPokemon.js
@@ -18,3 +18,24 @@ var pokemonEatingHabbits = function(snack1, snack2){
 // First parameter defines the scope of the function and the second one provides the parameter. So its a way of attaching
 // functionality to object
 pokemonEatingHabbits.apply(object, ["cheeze", "onions"]);
+
+
+
+
+
+// Example 2 .... No arguments to the functions
+sayHello = function(){
+  return "Hello"+this.name;
+}
+
+// lets define the context
+let person = {
+  name: "Shanur",
+  age:24
+}
+
+// now attach this object or give a context to sayHello function
+console.log(sayHello.call(person));
+sayHello();
+
+
