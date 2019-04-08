3eabd0db38f6f90fcd4fa1e95c9033681fed5c73
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-04-08 20:15:50 +0530

    Update bindPokemon.js

--- call-bind-apply/bindPokemon.js
+++ call-bind-apply/bindPokemon.js
@@ -1,13 +1,11 @@
-var object = {
-  firstName: "pika",
-  lastName: "chu"
-  
-  getPokemonName(){
-    return this.firstName+" "+this.lastName
-  }
+let obj = {
+  firstname: "Dev",
+  lastname: "kumar",
 }
 
+getFullName = function(lastname, lastname1){
+  return this.firstname+" "+this.lastname+" "+lastname+" "+lastname1;
+}
 
-
-
-console.log(object.getPokemonName);
+getFullName = getFullName.bind(obj);
+getFullName("Upadhyay", "Shanur")
