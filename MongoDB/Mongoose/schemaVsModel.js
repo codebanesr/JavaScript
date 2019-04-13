cbbba4d989022c9cf313ee4288634b3a8ebb3a46
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-04-14 02:15:13 +0530

    Create schemaVsModel.js

---
+++ MongoDB/Mongoose/schemaVsModel.js
@@ -0,0 +1,13 @@
+Schema is like an interface 
+Model is like a class which will implement that interface
+
+
+
+          CourseSchema
+               |
+      --------------------
+      |                   | 
+    nodeCourseModel    angularCourseModel
+    
+    
+    now we can create instances of nodeCourseModel and add it to the database collection
