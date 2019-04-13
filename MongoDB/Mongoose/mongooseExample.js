ffaa2cb78bf3a05cb572c0192a885d6102d5ea12
Shanur Rahman <shanur.cse.nitap@gmail.com>
2019-04-14 02:21:52 +0530

    Relationship between schema model and object

---
+++ MongoDB/Mongoose/mongooseExample.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+//This is like defining an interface, so now we can have 
+//models like nodeCouseModel which will implement this
+const courseSchema = mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: {type: Date, default: Date.now},
+  isPublished: Boolean
+})
+
+
+//now we have a model(lets just think of it as a class)
+const Course = mongoose.model('Course', courseSchema);
+
+
+
+//This is like an object that can be pushed into the database
+const course = new Course({
+  name: "Node.js course",
+  author: "Shanur",
+  tags:["node", "js"]
+  isPublished: true
+})
